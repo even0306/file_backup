f99a11161c7e4acaaecc958a34b4e5222741f367
even0306 <58162761+even0306@users.noreply.github.com>
2023-06-07 14:44:17 +0800

    Add files via upload

---
+++ file_backup_local.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# hosts.txt 书写格式
+#
+# /app
+# /home/abc/aaa
+#
+# 写需要备份的文件或文件夹路径，一行一条，末尾不要带“/”
+DATAS=$(cat /app/scripts/hosts.txt)
+
+BACKUP_PATH="/media/backup/"
+
+function dobackup() {
+  for DATA in ${DATAS[@]}; do
+    temp=${DATA}
+    NAME=$(echo ${DATA} | awk -F '\/' '{print $NF}')
+    DATE=$(date '+%Y-%m-%d')
+    FILEPATH=${NAME}"_"${DATE}
+	    
+    if [ ! -d ${BACKUP_PATH}/${NAME} ]; then
+      mkdir -p ${BACKUP_PATH}/${NAME}
+      chmod 777 ${BACKUP_PATH}/${NAME}
+      rsync -az --append-verify --copy-unsafe-links ${temp}/ ${BACKUP_PATH}/${NAME}/${FILEPATH}/
+      ln -s ${BACKUP_PATH}/${NAME}/${FILEPATH} ${BACKUP_PATH}/${NAME}/${NAME}"_latest"
+      exit 0
+    fi
+
+
+    rsync -az --delete-delay --append-verify --copy-unsafe-links --link-dest ${BACKUP_PATH}/${NAME}/${NAME}"_latest" ${temp}/ ${BACKUP_PATH}/${NAME}/${FILEPATH}/
+    wait
+    ISEXIST=$(ls ${BACKUP_PATH}/${NAME}/${FILEPATH}/ | wc -l)
+    if [ ${ISEXIST} -ne 0 ]; then
+      echo "${NAME} backup success"
+      rm -f ${BACKUP_PATH}/${NAME}/${NAME}"_latest"
+      ln -s ${BACKUP_PATH}/${NAME}/${FILEPATH} ${BACKUP_PATH}/${NAME}/${NAME}"_latest"
+      if [ ${BACKUP_PATH}x != ""x ];then
+        cd ${BACKUP_PATH}/${NAME}
+      else
+	echo "cannot find backup path!"
+	exit 1
+      fi
+      rm -rf $(ls -l | grep ${NAME}_ | head -n -8)
+      wait
+      cd $HOME
+    else
+      if [ ${BACKUP_PATH}x != ""x ];then
+        find ${BACKUP_PATH}/${NAME}/ -maxdepth 1 -type d -empty -delete
+      fi
+      echo "${NAME} backup failed"
+    fi
+  done
+}
+
+dobackup
---
+++ file_backup_remote.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# hosts.txt 书写格式
+#
+# 192.168.1.123：app:db:end
+#
+# 以“:”分隔，第一位写源主机ip，后面接着rsync服务中配置的要备份的模块，最后以end结尾，一行一条
+DATAS=$(cat ${BACKUP_PATH}/hosts.txt)
+
+BACKUP_PATH="/media/backup/"
+
+PASSWD_FILE="/etc/rsyncd.passwd"
+
+function dobackup() {
+  for DATA in ${DATAS[@]}; do
+    for ((i=0;i<15;i++)) {
+      temp=$(echo ${DATA} | awk -F ':' -v p="$[${i}+1]" '{print $p}')
+	  DATE=$(date '+%Y-%m-%d')
+	  FILEPATH=${temp}"_"${DATE}
+      if [ ${i} == 0 ]; then
+        if [ ! -d ${BACKUP_PATH}/${temp} ]; then
+          mkdir ${BACKUP_PATH}/${temp}
+	      chmod 777 ${BACKUP_PATH}/${temp}
+	    fi
+        HOST=${temp}
+		continue
+	  fi
+	    
+	  if [ "${temp}"x = "end"x ]; then
+        break
+	  fi
+
+      if [ ! -d ${BACKUP_PATH}/${HOST} ]; then
+      	mkdir -p ${BACKUP_PATH}/${HOST}/${FILEPATH}
+	chmod 777 ${BACKUP_PATH}/${HOST}/${FILEPATH}
+	rsync -az --append-verify --copy-unsafe-links --password-file=${PASSWD_FILE}  rsync@${HOST}::${temp}/ ${BACKUP_PATH}/${HOST}/${FILEPATH}/
+	ln -s ${BACKUP_PATH}/${HOST}/${FILEPATH} ${BACKUP_PATH}/${HOST}/${temp}"_latest"
+	exit 0
+      fi
+
+      mkdir -p ${BACKUP_PATH}/${HOST}/${FILEPATH}
+      chmod 777 ${BACKUP_PATH}/${HOST}/${FILEPATH}
+      rsync -az --delete-delay --append-verify --copy-unsafe-links --password-file=${PASSWD_FILE} --link-dest ${BACKUP_PATH}/${HOST}/${temp}"_latest"  rsync@${HOST}::${temp}/ ${BACKUP_PATH}/${HOST}/${FILEPATH}/
+	  wait
+	  ISEXIST=$(ls ${BACKUP_PATH}/${HOST}/${FILEPATH}/ | wc -l)
+	  if [ ${ISEXIST} -ne 0 ]; then
+	    echo "${HOST} backup success"
+	    rm -f ${BACKUP_PATH}/${HOST}/${temp}"_latest"
+	    ln -s ${BACKUP_PATH}/${HOST}/${FILEPATH} ${BACKUP_PATH}/${HOST}/${temp}"_latest"
+            cd ${BACKUP_PATH}/${HOST}/
+            rm -rf $(ls -l | grep ${temp}_ | head -n -8)
+	    wait
+            cd $HOME
+	  else
+	    find ${BACKUP_PATH}/${HOST}/ -maxdepth 1 -type d -empty -delete
+	    echo "${HOST} backup failed"
+	  fi
+	}
+  done
+}
+
+dobackup
